7835077e8fdbbd511a4bbfda88d56e1b3ec53967
myohtet003 <wmh@cbs.com.mm>
2025-06-04 23:11:15 +0630

    migration data

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
  */
-class ProjectFactory extends Factory
+class TaskFactory extends Factory
 {
     /**
      * Define the model's default state.
@@ -21,7 +21,9 @@ class ProjectFactory extends Factory
             'description' => fake()->realText(),
             'due_date' => fake()->dateTimeBetween('now', '+1 year'),
             'status' => fake()->randomElement(['pending','in_progress','completed']),
+            'priority' => fake()->randomElement(['low','medium', 'high']),
             'image_path' => fake()->imageUrl(),
+            'assigned_user_id' => 1,
             'created_by' => 1,
             'updated_by' => 1,
             'created_at' => now(),
--- resources/js/Pages/Project/Index.jsx
+++ resources/js/Pages/Project/Index.jsx
@@ -1,23 +1,24 @@
-import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { Head } from "@inertiajs/react";
+import React from "react";
 
-export default function Dashboard({auth}) {
+export default function Index({ auth, projects }) {
     return (
         <AuthenticatedLayout
             user={auth.user}
             header={
                 <h2 className="text-xl font-semibold leading-tight text-gray-800 dark dark:text-gray-200">
-                    Dashboard
+                    Projects
                 </h2>
             }
         >
-            <Head title="Dashboard" />
+            <Head title="Project" />
 
             <div className="py-12">
                 <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                         <div className="p-6 text-gray-900 dark:text-gray-100">
-                            You're logged in!
+                            projects
                         </div>
                     </div>
                 </div>
